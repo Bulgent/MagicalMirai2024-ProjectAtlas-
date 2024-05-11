9efc4f46cb79797efbe572f60679ab98be27e057
unknown <argynnini@gmail.com>
2024-05-12 07:06:33 +0900

    選択可能

--- src/LyricComponent.tsx
+++ src/LyricComponent.tsx
@@ -4,7 +4,6 @@ import { Player } from 'textalive-app-api';
 import songRead from './data/Song';
 import { PlayerControl } from './PlayerControl';
 
-// player.addListener({
 //   onAppReady, // APIの準備完了
 //   onVideoReady, // 楽曲情報取得完了
 //   onTimerReady, // 再生準備完了
@@ -14,41 +13,13 @@ import { PlayerControl } from './PlayerControl';
 //   onPause, // 楽曲一時停止時
 //   onStop, // 楽曲停止時
 //   onAppMediaChange, // 楽曲変更時
-// });
 
-// TO DO
-// 曲選択のボタン表示       ☑
-// 押されるまで待機         ☑
-// 押された曲データ読み込み ☒ Invalid hookエラーが出る
-// 再生                     ☑
-
-export function SelectSongConponent() {
-
-  const handleChangeSong = (readSongNum: number) => {
-
-      console.log(readSongNum);
-      LyricComponent(readSongNum)
-  };
-
-  return (
-      <>
-          <button type="button" onClick={() => handleChangeSong(0)}>SUPERHERO</button>
-          <button type="button" onClick={() => handleChangeSong(1)}>いつか君と話したミライは</button>
-          <button type="button" onClick={() => handleChangeSong(2)}>フューチャーノーツ</button>
-          <button type="button" onClick={() => handleChangeSong(3)}>未来交響曲</button>
-          <button type="button" onClick={() => handleChangeSong(4)}>リアリティ</button>
-          <button type="button" onClick={() => handleChangeSong(5)}>The Marks</button>
-      </>
-  );
-}
-
-function LyricComponent(songNum : number) {
+function LyricComponent() {
   const [player, setPlayer] = useState(null);
   const [app, setApp] = useState(null); //
   const [char, setChar] = useState(''); // 歌詞情報
-  const [chord, setChord] = useState(''); // 歌詞情報
-  //const [songNum, setSong] = useState(songNum);
-
+  const [chord, setChord] = useState(''); // コード情報
+  const [songNum, setSongNum] = useState(-1)
   // const [fontFamily, setFontFamily] = useState(sansSerif);
   // const [fontSize, setFontSize] = useState(defaultFontSize);
   // const [color, setColor] = useState(defaultColor);
@@ -63,16 +34,14 @@ function LyricComponent(songNum : number) {
 
   // 関数実行をReactのレンダリング後まで遅らせるやつ(上のdivが描画されるまで待機)
   useEffect(() => {
-    // 画面描画前のときはエラーが出るのでスキップ
-    if (typeof window === 'undefined' || !mediaElement) {
+    // 曲選択前または画面描画前のときはエラーが出るのでスキップ
+    if (songNum < 0 || typeof window === 'undefined' || !mediaElement) {
       return;
     }
-
     console.log('--- [app] create Player instance ---');
     const p = new Player({
       app: {
-        // トークンは https://developer.textalive.jp/profile で取得したものを使う
-        token: 'elLljAkPmCHHiGDP',
+        token: 'elLljAkPmCHHiGDP', // トークンは https://developer.textalive.jp/profile で取得したものを使う
       },
       mediaElement,
     });
@@ -81,21 +50,18 @@ function LyricComponent(songNum : number) {
       onAppReady: (app) => {
         console.log('--- [app] initialized as TextAlive app ---');
         console.log('managed:', app.managed);
-        console.log('host:', app.host);
-        console.log('song url:', app.songUrl);
-        if (!app.songUrl) {
-          p.createFromSongUrl(songRead[songNum].songURL, {
-            video: {
-              // 音楽地図訂正履歴
-              beatId: songRead[songNum].video.beatId,
-              chordId: songRead[songNum].video.chordId,
-              repetitiveSegmentId: songRead[songNum].video.repetitiveSegmentId,
-              // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FhZ35%2F20240130103028
-              lyricId: songRead[songNum].video.lyricId,
-              lyricDiffId: songRead[songNum].video.lyricDiffId,
-            },
-          });
-        }
+        // 選ばれた曲を読み込み
+        p.createFromSongUrl(songRead[songNum].songURL, {
+          video: {
+            // 音楽地図訂正履歴
+            beatId: songRead[songNum].video.beatId,
+            chordId: songRead[songNum].video.chordId,
+            repetitiveSegmentId: songRead[songNum].video.repetitiveSegmentId,
+            // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FhZ35%2F20240130103028
+            lyricId: songRead[songNum].video.lyricId,
+            lyricDiffId: songRead[songNum].video.lyricDiffId,
+          },
+        });
         setApp(app);
       },
       // onAppParameterUpdate: (name, value) => {
@@ -118,28 +84,27 @@ function LyricComponent(songNum : number) {
         console.log('--- [app] video is ready ---');
         console.log('player:', p);
         console.log('player.data.song:', p.data.song);
-        console.log('player.data.song.name:', p.data.song.name);
-        console.log('player.data.song.artist.name:', p.data.song.artist.name);
+        console.log('読込曲:', p.data.song.name, " / ", p.data.song.artist.name);
         console.log('player.data.songMap:', p.data.songMap);
+        // 一番最初の文字
         let c = p.video.firstChar;
+        // 前の歌詞と次の歌詞が同じ時はずっと繰り返す
         while (c && c.next) {
           c.animate = (now, u) => {
+            // 再生時間内の時
             if (u.startTime <= now && u.endTime > now) {
+              // 歌詞の更新
               setChar(u.text);
               console.log(p.findChord(p.timer.position).name)
               setChord(p.findChord(p.timer.position).name + " → " + p.findChord(p.timer.position).next.name);
             }
           };
+          // 次の文字
           c = c.next;
         }
       },
-      onAppMediaChange: (mediaUrl:string) => {
-        console.log("新しい再生楽曲が指定されました:", mediaUrl);
-      },
     };
     p.addListener(playerListener);
-
-
 
     setPlayer(p);
     return () => {
@@ -147,63 +112,77 @@ function LyricComponent(songNum : number) {
       p.removeListener(playerListener);
       p.dispose();
     };
-
-
   }, [mediaElement]);
 
-  return (
-    <>
-      {player && app && (
-        <div className="controls">
-          <PlayerControl disabled={app.managed} player={player} />
+  // 曲選択画面
+  if (songNum < 0) {
+    return (
+      <>
+        <button type="button" onClick={() => setSongNum(0)}>SUPERHERO</button>
+        <button type="button" onClick={() => setSongNum(1)}>いつか君と話したミライは</button>
+        <button type="button" onClick={() => setSongNum(2)}>フューチャーノーツ</button>
+        <button type="button" onClick={() => setSongNum(3)}>未来交響曲</button>
+        <button type="button" onClick={() => setSongNum(4)}>リアリティ</button>
+        <button type="button" onClick={() => setSongNum(5)}>The Marks</button>
+      </>
+    );
+  }
+  // 再生画面
+  else {
+    return (
+      <>
+        {player && app && (
+          <div className="controls">
+            <PlayerControl disabled={app.managed} player={player} />
+          </div>
+        )}
+        <div
+          className="wrapper"
+          style={{
+            background: '#fff',
+          }}
+        >
+          <div className="char">{char}</div>
+          <div className="chord">{chord}</div>
         </div>
-      )}
-      <div
-        className="wrapper"
-        style={{
-          background: '#fff',
-        }}
-      >
-        <div className="char">{char}</div>
-        <div className="chord">{chord}</div>
-      </div>
-      {/* <button type="button" onClick={() => handleChangeSong(0)}>SUPERHERO</button>
+        {/* <button type="button" onClick={() => handleChangeSong(0)}>SUPERHERO</button>
       <button type="button" onClick={() => handleChangeSong(1)}>いつか君と話したミライは</button>
       <button type="button" onClick={() => handleChangeSong(2)}>フューチャーノーツ</button>
       <button type="button" onClick={() => handleChangeSong(3)}>未来交響曲</button>
       <button type="button" onClick={() => handleChangeSong(4)}>リアリティ</button>
       <button type="button" onClick={() => handleChangeSong(5)}>The Marks</button> */}
-      {div}
-    </>
-  );
+        {div}
+      </>
+    );
 
-  // return (
-  //   <>
-  //     {player && app && (
-  //       <div className="controls">
-  //         <PlayerControl disabled={app.managed} player={player} />
-  //       </div>
-  //     )}
-  //     <div
-  //       className="wrapper"
-  //       style={{
-  //         background: darkMode ? '#333' : '#fff',
-  //       }}
-  //     >
-  //       <div
-  //         className="char"
-  //         style={{
-  //           fontFamily,
-  //           fontSize: `${fontSize}vh`,
-  //           color,
-  //         }}
-  //       >
-  //         {char}
-  //       </div>
-  //     </div>
-  //     {div}
-  //   </>
-  // );
+    // return (
+    //   <>
+    //     {player && app && (
+    //       <div className="controls">
+    //         <PlayerControl disabled={app.managed} player={player} />
+    //       </div>
+    //     )}
+    //     <div
+    //       className="wrapper"
+    //       style={{
+    //         background: darkMode ? '#333' : '#fff',
+    //       }}
+    //     >
+    //       <div
+    //         className="char"
+    //         style={{
+    //           fontFamily,
+    //           fontSize: `${fontSize}vh`,
+    //           color,
+    //         }}
+    //       >
+    //         {char}
+    //       </div>
+    //     </div>
+    //     {div}
+    //   </>
+    // );
+  }
 }
-// export default LyricComponent;
-export default SelectSongConponent;
+export default LyricComponent;
+// export default SelectSongConponent;
