bb4acd53711dafce2ca16aa03afeafb337263901
argynnini <argynnini@gmail.com>
2024-06-29 02:50:32 +0900

    addacredit

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -558,6 +558,7 @@ export const MapComponent = (props: any) => {
           attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
           url="https://tiles.stadiamaps.com/styles/stamen_terrain.json" // https://docs.stadiamaps.com/map-styles/osm-bright/ より取得
           ref={OSMlayerRef}
+          style={{ name: "Stadia Maps", version: 8, sources: {}, layers: [] }}
         />
         <MoveMapByRoute />
         <AddNotesToMap />
