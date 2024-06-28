005f945925e346e81a48981cdbc314c0d2d4fde0
argynnini <argynnini@gmail.com>
2024-06-29 02:15:00 +0900

    addacredit

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -4,11 +4,11 @@ import songData from '../utils/Song';
 import creditData from '../utils/credits';
 import '../styles/App.css';
 import '../styles/Welcome.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { msToMs } from '../utils/utils';
 
 export const WelcomePage = () => {
-    const [songIndex, setSongIndex] = useState(-1);
+    const songIndex = useRef(-1);
     const [songTitle, setSongTitle] = useState<string>('MAGICAL MIRAI');
     const [songArtist, setSongArtist] = useState<string>('Programming Contest');
     const [songVocaloid, setSongVocaloid] = useState<string>('Project ATLAS');
@@ -23,28 +23,43 @@ export const WelcomePage = () => {
 
     // マウスがボタンに乗った時の処理
     const handleMouseEnter = (index: number) => {
-        setSongIndex(index);
+        songIndex.current = index;
         setSongTitle(songData[index].title);
         setSongArtist(songData[index].artist);
         setSongVocaloid(songData[index].vocaloid.japanese);
         setSongLength(songData[index].duration);
     };
 
+    window.addEventListener('orientationchange', function () {
+        if (window.orientation === 0) {
+            // 縦向きの場合
+            document.querySelector('.rotate-device').style.display = 'block';
+        } else {
+            // 横向きの場合
+            document.querySelector('.rotate-device').style.display = 'none';
+        }
+    });
+
     return (
         <div id="display" className="soft-gloss">
             <div id="navi" className="split">
+                <div className="rotate-device">
+                    画面を横向きにしてください．
+                    <br />
+                    Please rotate the screen horizontally.
+                </div>
                 <div className="jacket-song">
                     <div className="grid-song">
                         <div className='select-song-text'>Select Song</div>
                         {/* ボタンをsongData分追加 */}
                         {songData.map((song, index) => (
-                            <button key={index} className='select-song-button' onClick={() => goToGamePage(index)} onMouseEnter={() => handleMouseEnter(index)}>
+                            <button key={index} className='select-song-button' onClick={() => goToGamePage(index)} onMouseEnter={() => handleMouseEnter(index)} onTouchStart={() => handleMouseEnter(index)}>
                                 {index + 1}.{song.title}
                             </button>
                         ))}
                     </div>
                     <div className='selectSongInfo'>
-                        <img className='selectJacket' src={songIndex != -1 ? getImage(songIndex) : 'src/assets/images/project.png'} alt='' />
+                        <img className='selectJacket' src={songIndex.current != -1 ? getImage(songIndex.current) : 'src/assets/images/project.png'} alt='' />
                         <div className='selectTitle'>{songTitle}</div>
                         <div className='selectArtist'>{songArtist}</div>
                         <div className='selectVocaloid'>{songVocaloid}</div>
