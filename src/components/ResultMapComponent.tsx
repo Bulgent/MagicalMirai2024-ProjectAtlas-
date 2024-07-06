965c55e7e24303ad1b5d4c552ea5315ba3f76000
Bulgent <moritakakuro0305@gmail.com>
2024-07-06 14:56:27 +0900

    feat: mapから文字を消す処理を追加

--- src/components/ResultMapComponent.tsx
+++ src/components/ResultMapComponent.tsx
@@ -3,7 +3,7 @@ import { MapLibreTileLayer } from '../utils/MapLibraTileLayer.ts'
 import areas from '../assets/jsons/map_data/area.json'
 import { mapStyle} from '../utils/MapStyle.ts'
 import { LatLngLiteral, MaplibreGL } from 'leaflet';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const MapComponent = (props: any) => {
     const mapZoom = 10.2;
@@ -11,48 +11,60 @@ export const MapComponent = (props: any) => {
     // OpenStreetMapレイヤー
     const OSMlayerRef = useRef<MaplibreGL | null>(null);
     const isInitMapRef = useRef<Boolean>(true);
-  /**
-   * Mapから文字を消す処理  
-   */
+    const [isMapReady, setIsMapReady] = useState(false);
+
+    // MapLibreTileLayerのrefを定期監視する処理
     useEffect(() => {
-        console.log(OSMlayerRef.current)
-        if (!isInitMapRef.current) {
-        return
+        if (!isInitMapRef.current){
+            return
         }
-        if (OSMlayerRef.current) {
-        // 読み込みが2段階ある
-        if (OSMlayerRef.current.getMaplibreMap().getStyle() === undefined) {
-          return
+        const interval = setInterval(() => {
+            console.log("checking")
+            if (OSMlayerRef.current) {
+                if (OSMlayerRef.current.getMaplibreMap().getStyle()){
+                    console.log(OSMlayerRef.current)
+                    setIsMapReady(true);
+                    clearInterval(interval);
+                }
+            }
+        }, 10);
+        return () => clearInterval(interval);
+    }, []);
+
+    // MapLibraTileLayerのrefがnullから変化した場合に行う処理
+    useEffect(() => {
+        if (!isInitMapRef.current || !OSMlayerRef.current) {
+            return
         }
-        const osmMap = OSMlayerRef.current.getMaplibreMap();
-        // ここでスタイルを変更
-        osmMap.getStyle().layers.forEach((l: any) => {
-          if (l.type === "symbol") osmMap.setLayoutProperty(l.id, "visibility", "none"); // 文字を消す
-          // 水の色を変更
-          if (["waterway", "water"].includes(l.id) && l.type === "fill") {
-            osmMap.setPaintProperty(l.id, "fill-color", "#90dbee");
-          }
-          // 道路の色を変更
-          if (l["source-layer"] === "transportation" && l.type === "line") {
-            osmMap.setPaintProperty(l.id, "line-color", "#8995a2");
-          }
-        });
-        isInitMapRef.current = false
-      }
-    }, [OSMlayerRef.current]);
+        if (OSMlayerRef.current.getMaplibreMap().getStyle()){
+            const osmMap = OSMlayerRef.current.getMaplibreMap();
+            // ここでスタイルを変更
+            osmMap.getStyle().layers.forEach((l: any) => {
+              if (l.type === "symbol") osmMap.setLayoutProperty(l.id, "visibility", "none"); // 文字を消す
+              // 水の色を変更
+              if (["waterway", "water"].includes(l.id) && l.type === "fill") {
+                osmMap.setPaintProperty(l.id, "fill-color", "#90dbee");
+              }
+              // 道路の色を変更
+              if (l["source-layer"] === "transportation" && l.type === "line") {
+                osmMap.setPaintProperty(l.id, "line-color", "#8995a2");
+              }
+            });
+            isInitMapRef.current = false
+        }
+    }, [isMapReady]);
 
 
     return(
         <MapContainer className='mapcomponent' style={{ backgroundColor: '#f5f3f3' }}
         center={mapCenter} zoom={mapZoom}
-        minZoom={mapZoom} maxZoom={mapZoom}
+        // minZoom={mapZoom} maxZoom={mapZoom}
         zoomSnap={0.1} zoomDelta={0.5} trackResize={false}
         inertiaMaxSpeed={500} inertiaDeceleration={1000}
         zoomControl={false} attributionControl={false}
         maxBoundsViscosity={1.0}
         preferCanvas={true}
         boxZoom={false} doubleClickZoom={false}
-        inertia={false} dragging={false}
         >
         <MapLibreTileLayer
           attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
