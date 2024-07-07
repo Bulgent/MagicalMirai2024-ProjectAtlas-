6413e3fafe7809e5ae7568f3e85e4d5f03c9c2c6
Bulgent <moritakakuro0305@gmail.com>
2024-07-07 21:35:03 +0900

    apikey修正

--- src/components/ResultDetailMapComponent.tsx
+++ src/components/ResultDetailMapComponent.tsx
@@ -211,7 +211,7 @@ export const ResultDetailMapComponent = (props: any) => {
             <VisitedPoints />
             <MapLibreTileLayer
                 attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
-                url="https://tiles.stadiamaps.com/styles/stamen_terrain.json?api_key=1a4eedc0-5153-47ea-b4ab-3e93813e99b0" // https://docs.stadiamaps.com/map-styles/osm-bright/ より取得
+                url={`https://tiles.stadiamaps.com/styles/stamen_terrain.json?api_key=${process.env.MAP_API_KEY}`} // https://docs.stadiamaps.com/map-styles/osm-bright/ より取得
                 ref={OSMlayerRef}
                 style={{ name: "Stadia Maps", version: 8, sources: {}, layers: [] }}
             />
